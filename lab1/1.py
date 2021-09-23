6fb2226b3aac0e91943693a76fb91baf30ed9523
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2021-09-23 09:52:30 +0300

    changed 1, 2 task

--- lab1/1.py
+++ lab1/1.py
@@ -2,7 +2,12 @@ import sys
 
 print(sys.argv)
 arguments = sys.argv[1:]
-a = int(arguments[0])
-b = str(arguments[1])
-c = int(arguments[2])
-print('Result: ', eval(str(a) + b + str(c)))
+try:
+    a = int(arguments[0])
+    b = str(arguments[1])
+    c = int(arguments[2])
+    print('Result: ', eval(str(a) + b + str(c)))
+except ZeroDivisionError:
+    print("Error. Division by zero")
+except:
+    print("Error. Incorrectly entered data")
--- lab1/2.py
+++ lab1/2.py
@@ -1,37 +1,21 @@
 import argparse
+import operator
 
-parser = argparse.ArgumentParser()
-parser.add_argument("action", type=str)
-parser.add_argument("a", type=int)
-parser.add_argument("b", type=int)
-args = parser.parse_args()
-print(args)
+func = {"add": operator.add,
+        'mull': operator.mul,
+        'sub': operator.sub,
+        'div': operator.truediv
+        }
+try:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", type=str)
+    parser.add_argument("a", type=int)
+    parser.add_argument("b", type=int)
+    args = parser.parse_args()
+    print(args)
 
-
-def add(a, b):
-    print(a + b)
-
-
-def mul(a, b):
-    print(a - b)
-
-
-def sub(a, b):
-    print(a * b)
-
-
-def div(a, b):
-    if b != 0:
-        print(a / b)
-    else:
-        print("Error")
-
-
-if args.action == "add":
-    add(args.a, args.b)
-elif args.action == "mul":
-    mul(args.a, args.b)
-elif args.action == "sub":
-    sub(args.a, args.b)
-elif args.action == "div":
-    div(args.a, args.b)
+    print(func[args.action](args.a, args.b))
+except ZeroDivisionError:
+    print("Error. Division by zero")
+except:
+    print("Error. Incorrectly entered data")
