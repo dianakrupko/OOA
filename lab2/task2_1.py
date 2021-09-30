2a5a652063c8e9425bf34c1c516117098e133e1f
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2021-09-30 23:34:46 +0300

    changed task2_1

--- lab2/task2_1.py
+++ lab2/task2_1.py
@@ -6,9 +6,13 @@ class Rectangle:
 
     def set_width(self, width):
         self.width = width
+        if not type(self.width) is float or self.width < 0.0 or self.width > 20.0:
+            print("Error")
 
     def set_length(self, length):
         self.length = length
+        if not type(self.length) is float or self.length < 0.0 or self.length > 20.0:
+            print("Error")
 
     def get_width(self):
         return self.width
@@ -18,6 +22,9 @@ class Rectangle:
 
     def get_area(self):
         return self.length * self.width
+
+    def get_perimeter(self):
+        return 2 * (self.length + self.width)
 
     def str(self):
         return 'length = {}, width = {}'.format(self.length, self.width)
